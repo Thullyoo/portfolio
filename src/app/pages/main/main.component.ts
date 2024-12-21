22f1d34b41c985cdde23f62da4c0e881f4253300
Thullyoo <thullyofilho@gmail.com>
2024-12-21 17:41:53 -0300

    fix: remove unecessary var

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { AboutMeComponent } from '../../components/about-me/about-me.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, AboutMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent{
  
-  quantidadeEstrela = 20;
 }
