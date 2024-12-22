ec846b0a0948f0e758fddb7717494aa81797fb9b
Thullyoo <thullyofilho@gmail.com>
2024-12-22 20:17:11 -0300

    feat: add my projects on main component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { AboutMeComponent } from '../../components/about-me/about-me.component';
+import { MyProjectsComponent } from '../../components/my-projects/my-projects.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent],
+  imports: [HeaderComponent, AboutMeComponent, MyProjectsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
