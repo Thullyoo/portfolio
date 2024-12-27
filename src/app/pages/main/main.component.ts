5aae7dea6280560d9a1514b93a66530025e529b7
Thullyoo <thullyofilho@gmail.com>
2024-12-27 13:43:58 -0300

    feat: add footer component on main component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { AboutMeComponent } from '../../components/about-me/about-me.component';
 import { MyProjectsComponent } from '../../components/my-projects/my-projects.component';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent, MyProjectsComponent],
+  imports: [HeaderComponent, AboutMeComponent, MyProjectsComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
